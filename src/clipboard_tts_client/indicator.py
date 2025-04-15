b4a5b25e6b3f4158a1a6a04d3c98efc18dd94514
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-04-15 10:03:41 -0300

    mejorando elc odigo para que sea mas padronizado a otros de su familia

--- src/clipboard_tts_client/indicator.py
+++ src/clipboard_tts_client/indicator.py
@@ -6,57 +6,56 @@ import signal
 from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
 from PyQt5.QtGui import QIcon
 
-from clipboard_tts_client.lib_funcs import tts_remove_task, tts_play  # Removi o ponto antes do import
+from clipboard_tts_client.lib_funcs import tts_remove_task, tts_play  # Ajuste se estiver num pacote
 
-last_play_id = None
+class ClipboardTtsClientApp(QApplication):
+    def __init__(self, sys_argv):
+        super().__init__(sys_argv)
+        self.last_play_id = None
+        self.menu = None
+        self.init_tray()
 
-def play():
-    global last_play_id
-    app = QApplication.instance()
-    if app is None:
-        app = QApplication([])
+    def init_tray(self):
+        # Ícone
+        icon_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'logo.png')
+        self.tray_icon = QSystemTrayIcon(QIcon(icon_path), self)
 
-    clipboard = app.clipboard()
-    text = clipboard.text()
-    last_play_id = tts_play(text)
+        # Menu
+        self.menu = QMenu()
 
-def remove():
-    global last_play_id
-    msg = tts_remove_task(last_play_id)
-    # print(msg)
+        self.play_action = QAction("Play clipboard")
+        self.play_action.triggered.connect(self.play_clipboard)
+        self.menu.addAction(self.play_action)
 
-def quit_app():
-    QApplication.quit()
+        self.remove_action = QAction("Remove last task")
+        self.remove_action.triggered.connect(self.remove_last_task)
+        self.menu.addAction(self.remove_action)
+
+        self.quit_action = QAction("Exit")
+        self.quit_action.triggered.connect(self.quit)
+        self.menu.addAction(self.quit_action)
+
+        self.tray_icon.setContextMenu(self.menu)
+        self.tray_icon.show()
+
+    def play_clipboard(self):
+        clipboard = self.clipboard()
+        text = clipboard.text()
+        self.last_play_id = tts_play(text)
+
+    def remove_last_task(self):
+        if self.last_play_id is not None:
+            tts_remove_task(self.last_play_id)
+
+    def quit(self):
+        self.exit()
 
 def main():
-    app = QApplication(sys.argv)
-
-    # Criar o ícone da bandeja
-    tray_icon = QSystemTrayIcon()
-    icon_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icons', 'logo.png')
-    tray_icon.setIcon(QIcon(icon_path))
-
-    # Criar o menu da bandeja
-    menu = QMenu()
-
-    play_action = QAction("Play clipboard")
-    play_action.triggered.connect(play)
-    menu.addAction(play_action)
-
-    remove_action = QAction("Remove last task")
-    remove_action.triggered.connect(remove)
-    menu.addAction(remove_action)
-
-    quit_action = QAction("Exit")
-    quit_action.triggered.connect(quit_app)
-    menu.addAction(quit_action)
-
-    tray_icon.setContextMenu(menu)
-    tray_icon.show()
-
-    # Capturar Ctrl+C no terminal
+    # Captura de sinal Ctrl+C no terminal
     signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+    app = ClipboardTtsClientApp(sys.argv)
+    app.setApplicationName("clipboard_tts_client") # xprop WM_CLASS # *.desktop -> StartupWMClass  
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
